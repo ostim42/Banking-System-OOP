be18b0f73b573e17ff9f16f0684b1bc204061b4a
aharp46 <164930442+aharp46@users.noreply.github.com>
2024-03-25 15:25:52 -0400

    Create Account.cpp

---
+++ Account.cpp
@@ -0,0 +1,15 @@
+#include "Account.h"
+
+Account::Account(double b, int n, std::string a) {
+	balance = b;
+	accountNumber = n;
+	accountHolder = a;
+	charges = 0;
+
+}
+void Account::Deposit(double a) {
+	balance += a;
+}
+void Account::Withdraw(double a) {
+	balance -= a;
+}
