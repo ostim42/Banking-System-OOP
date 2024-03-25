4661f0ec37042d43199495ebe9976a0061b8e759
ostim42 <163911273+ostim42@users.noreply.github.com>
2024-03-25 19:36:17 -0400

    Create CheckingAccount.cpp

---
+++ CheckingAccount.cpp
@@ -0,0 +1,10 @@
+#include "CheckingAccount.h"
+#include <iostream>
+
+CheckingAccount::CheckingAccount(std::string id, double initialBalance, int days)
+    : Account(id, initialBalance), daysBetweenPaychecks(days) {}
+
+void CheckingAccount::print() const {
+    Account::print();
+    std::cout << "Days Between Paychecks: " << daysBetweenPaychecks << std::endl;
+}
