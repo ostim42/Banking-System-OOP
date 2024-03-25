f53e99a6607b56f2e2034649c50cc788ce1ee146
ostim42 <163911273+ostim42@users.noreply.github.com>
2024-03-25 19:36:02 -0400

    Create CheckingAccount.h

---
+++ CheckingAccount.h
@@ -0,0 +1,15 @@
+#ifndef CHECKINGACCOUNT_H
+#define CHECKINGACCOUNT_H
+
+#include "Account.h"
+
+class CheckingAccount : public Account {
+private:
+    int daysBetweenPaychecks;
+
+public:
+    CheckingAccount(std::string id, double initialBalance, int days);
+    void print() const override;
+};
+
+#endif // CHECKINGACCOUNT_H
