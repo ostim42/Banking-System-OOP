dc1852bd9477fa978d7a8226a797a0491764d329
NCYL8 <164215128+NCYL8@users.noreply.github.com>
2024-03-26 19:24:04 -0400

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -1,1 +1,13 @@
 //transaction.h
+
+class Transaction {
+public:
+  double amount;
+  double balance;
+  double transaction[];
+  double date;
+  void Withdraw();
+  void Deposit();
+  void CcStatement();
+};
+  
