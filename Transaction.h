41c807d1f097cb6949c55df41f3fc17ffbbdbedd
NCYL8 <164215128+NCYL8@users.noreply.github.com>
2024-03-27 12:12:21 -0400

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -2,12 +2,12 @@
 
 class Transaction {
 private:
-  double date[100];
-  double payments[100];
+  double date;
+  double payments;
   double credits;
   int n;
 public:
-  Transaction(double d[100], double pay[100]);
+  Transaction(double d, double pay);
   void Payment();
 };
   
