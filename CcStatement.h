fcb0808be0a4cae0da67aee0eb1f9afed54684b4
NCYL8 <164215128+NCYL8@users.noreply.github.com>
2024-03-30 22:25:31 -0400

    Update CcStatement.h

--- CcStatement.h
+++ CcStatement.h
@@ -4,6 +4,8 @@
 #include "Account.h"
 
 class CcStatement: public Transaction, public Account {
+private: 
+string text;
 public:
   CcStatement();
   CcStatement(double d, double pay, double creds, int AccNum double bal, double charge);
