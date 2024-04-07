cd691bd19fd785285ef454aeea38e8a10e9459fc
aharp46 <164930442+aharp46@users.noreply.github.com>
2024-04-06 21:57:01 -0400

    Update Account.h

--- Account.h
+++ Account.h
@@ -12,6 +12,6 @@ public:
 	Account(double b, int n, std::string a);
 	virtual void Deposit(double a);
 	virtual void Withdraw(double a);
-	virtual void print(double b, double c, std::string a, int n);
+	virtual void print();
 
 };
